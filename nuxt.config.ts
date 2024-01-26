672ce0213d9adc7074227dfe4c32273f12b95460
nobokko <nobokko+github@gmail.com>
2024-01-26 10:34:49 +0000

    feat: install tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   app: {
     baseURL: '/ts_danbooru-tag_editor/',
     buildAssetsDir: 'assets',
+  },
+  components: {
+    dirs: [{ path: './src/components', watch: true, }]
+  },
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
   },
 })
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,vue,ts}",
+    // "./layouts/**/*.vue",
+    // "./pages/**/*.vue",
+    // "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
