102a65bbf10b73267b1d34720a9a6d4266fb7d0c
nobokko <nobokko+github@gmail.com>
2024-01-26 23:49:56 +0900

    try: change setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   app: {
     baseURL: '/ts_danbooru-tag_editor/',
+    cdnURL: '/ts_danbooru-tag_editor/',
     buildAssetsDir: 'assets',
   },
   ssr: false, // サーバーは使用不可
